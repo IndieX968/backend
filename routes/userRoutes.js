2e508de8dde3b59ea0a9ef94a83383d9fdcf93ce
IndieX968 <appm4196@gmail.com>
2025-02-09 17:47:02 +0500

    Added Profile Apis

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,8 +4,11 @@ const {
   signInUser,
   updateUserRole,
   createStore,
+  editStore,
+  updateUserProfile,
 } = require("../controllers/userController");
 const tokenValidator = require("../middlewares/errorHandler");
+const upload = require("../middlewares/multer");
 
 const router = express.Router();
 
@@ -16,6 +19,15 @@ router.post("/signin", signInUser);
 
 router.put("/update-role", tokenValidator, updateUserRole);
 
-router.post("/store", tokenValidator, createStore);
+router.post("/store", tokenValidator, upload.single("image"), createStore);
+
+router.put("/update-store", tokenValidator, upload.single("image"), editStore);
+
+router.put(
+  "/update-profile",
+  tokenValidator,
+  upload.single("image"),
+  updateUserProfile
+);
 
 module.exports = router;
