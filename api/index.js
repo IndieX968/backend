459a9b12c046bab0d03e8cc1d1b1e30023164537
IndieX968 <appm4196@gmail.com>
2025-04-11 00:24:53 +0500

    Refactor API to use serverless-http and remove direct server listening

--- api/index.js
+++ api/index.js
@@ -1,8 +1,4 @@
 const app = require("../app");
+const serverless = require("serverless-http");
 
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = serverless(app);
